57c0fbdb41cf4ee98fcb257cffbfbe1bafa56020
DanielePiccioni <danielepiccioni1@gmail.com>
2025-12-11 12:51:51 +0100

    create layout base and insert the route on 2 page homepage and moviedetailpage for a single movie

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DefaultLayout from "./layouts/DefaultLayout"
+import HomePage from "./Pages/HomePage"
+import MovieDetailPage from "./Pages/MovieDetailPage"
+
+
 function App() {
   return (
     <>
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route element={<DefaultLayout />}>
+
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/movie/:id" element={<MovieDetailPage />} />
+
+          </Route>
+
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
