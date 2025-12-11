d18b9bc7bb5f515cfeabe6828708a005c21a53ce
DanielePiccioni <danielepiccioni1@gmail.com>
2025-12-11 16:24:44 +0100

    setup CORS in background and add with call axios in homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function HomePage() {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/movies")
+            .then(res => {
+                console.log("Film ottenuti dal server:", res.data);
+                setMovies(res.data);
+            })
+            .catch(err => console.error(err));
+    }, []);
+
     return (
-        <h1>Welcome to my meow</h1>
-    )
+        <>
+            <h1>Lista Meowilm</h1>
+
+            {movies.map(movie => (
+                <div key={movie.id}>
+                    <h3>{movie.title}</h3>
+                </div>
+            ))}
+        </>
+    );
 }
